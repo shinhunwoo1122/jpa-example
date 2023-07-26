11ea6cde59dc44177391a55fc87f42b5939bfa5b
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-07-26 17:40:29 +0900

    @ManyToOne 다대일 주인 엔티티 설정 및 @OneToMany 양방향 mappedBy 설정 정리 @OneToMany 일대다 실무에서 사용 잘 안함 반영되는 테이블이 반대쪽에 있기 떄문에 혼란을 줄 수 있고 또한 외래키를 트랜잭션 커밋전에 알 수 없기때문에 commit이후에 update처리 및 성능에 문제가 됨 @OneToOne 주엔티티에 설정시 성능향상과 지연로딩등을 사용 할 수 있음 하지만 데이터 변경으로 인해 1:1관계가 무너질시 대폭 수정해야하는 단점이 있음 참조엔티티에 설정시 외래키가 있는지 계속 봐야되기떄문에 즉시 로딩이 불가능 범용성 및 확장에는 더욱 유효한 방법임 @ManyToMany 실무에서 사용x 이유로는 참조테이블에는 컬럼추가 및 변경사항이 많기때문에 실무에서는 엔티티로 승격해서 @ManyToOne으로 풀어서 쓰기를 권장 함.
    @Inheritance(strategy = InheritanceType.SINGLE_TABLE) 추가함
    @MappedSuperclass 공통 속성 처리 추가함
    
    지연로딩 전체 추가함
    @ManyToOne, @OneToOne에 fetch = FetchType.LAZY추가함
    
    영속성전이 casecade = CaseCadeType.ALL 추가

--- src/main/java/org/example/jpashop/domain/Delivery.java
+++ src/main/java/org/example/jpashop/domain/Delivery.java
@@ -1,9 +1,8 @@
 package org.example.jpashop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 public class Delivery extends BaseEntity{
@@ -15,8 +14,9 @@ public class Delivery extends BaseEntity{
     private String zipcode;
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
+
 
 
 }
